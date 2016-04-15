7edfdd4446bb412b68233fb63cbe550a55a09b08
Kota Ishimoto <ishikota086@gmail.com>
2016-04-15 17:33:38 +0900

    Add validation on room

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,5 @@
 class Room < ApplicationRecord
+  validates :name, presence: true
+  validates :max_round, numericality: { greater_than: 0 }
+  validates :player_num, numericality: { greater_than: 0 }
 end
--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Room, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:room) { FactoryGirl.create(:room) }
+
+  describe "validation" do
+
+    describe "on name" do
+      it "should reject empty name room" do
+        expect { room.name = '' }.to change { room.valid? }.to false
+      end
+    end
+
+    describe "on max_round" do
+      context "when max round is zero" do
+        it "should reject" do
+          expect { room.max_round = 0 }.to change { room.valid? }.to false
+        end
+      end
+      context "when max round is negative" do
+        it "should reject" do
+          expect {room.max_round = -1 }.to change { room.valid? }.to false
+        end
+      end
+    end
+
+    describe "on player_num" do
+      context "when player number is zero" do
+        it "should reject" do
+          expect { room.player_num = 0 }.to change { room.valid? }.to false
+        end
+      end
+      context "when player number is negative" do
+        it "should reject" do
+          expect { room.player_num = -1 }.to change { room.valid? }.to false
+        end
+      end
+    end
+
+  end
 end
