36ff3476abf0a074aa36b18e91fd0fcb9c6b05b4
Kota Ishimoto <ishikota086@gmail.com>
2016-04-15 15:06:19 +0900

    Divide channel by room

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -8,6 +8,14 @@ class RoomChannel < ApplicationCable::Channel
     # Any cleanup needed when channel is unsubscribed
   end
 
+  def enter_room(data)
+    stream_from "room:#{data['room_id']}"
+  end
+
+  def speak_in_room(data)
+    ActionCable.server.broadcast "room:#{data['room_id']}", message: data['message']
+  end
+
   def declare_action(data)
     ActionCable.server.broadcast 'room_channel', message: data['message']
   end
