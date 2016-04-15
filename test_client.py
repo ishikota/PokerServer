cdd035c5a341a563af804dec9f3a082261e003f3
Kota Ishimoto <ishikota086@gmail.com>
2016-04-15 16:06:47 +0900

    Refactor by params builder

--- test_client.py
+++ test_client.py
@@ -1,12 +1,10 @@
 import websocket
 
-# json parameter
-identifier = r'"identifier":"{\"channel\":\"RoomChannel\"}"'
-cma = ' , '
-command = '"command" : '
-data = '"data" : '
-action = r'\"action\" : '
-auth_params = r'\"room_id\" : 1, \"user_id\" : 1, \"credencial\": \"fugafuga\"'
+# const
+host = "ws://localhost:3000/cable"
+room_id = 1
+user_id = 1
+credencial = "fugafuga"
 
 def on_message(ws, message):
     print message
@@ -20,19 +18,56 @@ def on_close(ws):
 def on_open(ws):
   while True:
     f = raw_input("s: subscribe, e: enter_room, m: send message, other: go listening loop")
+    params = ''
     if f == 's':
-      ws.send('{' + identifier + cma + command + '"subscribe"' + cma + data + '"{'+ auth_params +'}"'+ '}')
+      params = build_subscribe_params()
     elif f == 'e':
-      ws.send('{' + identifier + cma + command + '"message"'   + cma + data + '"{'+ auth_params + cma + action + r'\"enter_room\"' + '}"' + '}')
+      params = build_message_params("enter_room")
     elif f == 'm':
-      ws.send('{' + identifier + cma + command + '"message"'   + cma + data + '"{'+ auth_params + cma + action + r'\"speak_in_room\"' + cma +  r'\"message\" : \"hoge\"' + '}"' + '}')
+      params = build_message_params("speak_in_room", { "message" : "hoge" })
     else:
       break
+    ws.send(params)
 
+def build_subscribe_params():
+  return build_my_params("subscribe")
+
+def build_message_params(action, data={}):
+  return build_my_params("message", action, data)
+
+def build_my_params(command, action = '', data={}):
+  return build_params(room_id, user_id, credencial, command, action, data)
+
+def build_params(room_id, user_id, credencial, command, action = '', data={}):
+  return \
+     '{' + \
+        r'"identifier":"{\"channel\":\"RoomChannel\"}"' + ' , '\
+        r'"command" : "' + command + '" , ' + \
+        r'"data" : "{' + \
+          build_action(action) + \
+          build_data(data) + \
+          r'\"room_id\"    : ' + str(room_id)    + ' , ' + \
+          r'\"user_id\"    : ' + str(user_id)    + ' , ' + \
+          r'\"credencial\" : \"' + credencial +  r'\"'\
+        r'}"' + \
+      '}'
+
+def build_action(act):
+  if act == '':
+    return act
+  else:
+    return r'\"action\" : ' + r'\"' + act + r'\" , '
+
+def build_data(dt):
+  items = [r'\"' + key + r'\"' + ' : ' + r'\"' + val + r'\"' for key, val in dt.items()]
+  params = ' , '.join(items)
+  if params != '':
+    params = params + ' , '
+  return params
 
 if __name__ == "__main__":
     websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("ws://localhost:3000/cable",
+    ws = websocket.WebSocketApp(host,
                               on_message = on_message,
                               on_error = on_error,
                               on_close = on_close)
