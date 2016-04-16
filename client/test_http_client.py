9ac9fcd0d24190732299d33a9a4625b836865323
Kota Ishimoto <ishikota086@gmail.com>
2016-04-16 15:24:10 +0900

    Cut out lobby action logic from client

--- client/test_http_client.py
+++ client/test_http_client.py
@@ -1,34 +1,12 @@
-from http_client import CustomHttpClient
+from lobby_helper import LobbyHelper
 
 host = 'http://localhost:3000/'
-url = host + 'api/v1/'
-
-c = CustomHttpClient()
-
-def show_rooms(status=''):
-  params = { 'status' : status }
-  return c.get(url + 'rooms', params)
-
-def create_player(name):
-  params = { 'player' : { 'name' : name } }
-  r = c.post(url + 'players', params)
-  return r.json()
-
-def create_room(name, max_round, player_num):
-  params = { 'room' : { 'name' : name, 'max_round' : max_round, 'player_num' : player_num } }
-  r = c.post(url + 'rooms', params)
-  return r.json()
-
-def destroy_player(pid):
-  return c.delete(url + 'players/' + str(pid))
-
-def destroy_room(rid):
-  return c.delete(url + 'rooms/' + str(rid))
+domain = host + 'api/v1/'
 
 if __name__ == '__main__':
-  player = create_player('kota')
-  room = create_room('pokapoka', 5, 3)
-  show_rooms()
-  destroy_room(room['id'])
-  destroy_player(player['id'])
-
+  lobby = LobbyHelper(domain)
+  player = lobby.create_player('kota')
+  room = lobby.create_room('pokapoka', 5, 3)
+  lobby.show_rooms()
+  lobby.destroy_room(room['id'])
+  lobby.destroy_player(player['id'])
