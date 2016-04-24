e67d9638e807718c1cb02a73e146385548e9dbae
Kota Ishimoto <ishikota086@gmail.com>
2016-04-24 11:47:46 +0900

    Create deck blueprint

---
+++ spec/poker_engine/deck_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Deck do
+
+  let(:deck) { Deck.new }
+
+  describe "#draw_card" do
+
+    it "should return a card and remove it from deck"
+
+  end
+
+  describe "#draw_cards" do
+
+    it "should return cards and remove them from deck"
+
+  end
+
+end
+
