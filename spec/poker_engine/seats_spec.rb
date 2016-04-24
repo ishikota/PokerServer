488dbd33dced5a00e8c0c53ca7579027957d75d1
Kota Ishimoto <ishikota086@gmail.com>
2016-04-24 11:01:23 +0900

    Create seats blue print

---
+++ spec/poker_engine/seats_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Seats do
+
+  describe "#sit_down" do
+
+    it "should set player"
+
+  end
+
+  describe "#size" do
+
+    it "should return the number of players who sit on"
+
+  end
+
+  describe "#collect_bet" do
+
+    it "should collect bet from second player"
+
+  end
+
+  describe "#deactivate" do
+
+    it "should deactivate specified player"
+
+  end
+
+end
+
