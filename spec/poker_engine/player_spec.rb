1de4569a09c3396ec38572f299cfe098e1f9ddc0
Kota Ishimoto <ishikota086@gmail.com>
2016-04-24 11:30:13 +0900

    Create player blueprint

---
+++ spec/poker_engine/player_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Player do
+
+  describe "#collect_bet" do
+
+    it "should collect bet from player's stack"
+
+  end
+
+  describe "#deactivate" do
+
+    it "should deactivate player"
+
+  end
+
+end
+
