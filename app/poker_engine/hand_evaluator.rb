2cfeb7a052b247dc426d9b68935f7f1aef853052
Kota Ishimoto <ishikota086@gmail.com>
2016-04-25 17:39:13 +0900

    Refactor HandEvaluator

--- app/poker_engine/hand_evaluator.rb
+++ app/poker_engine/hand_evaluator.rb
@@ -23,13 +23,14 @@ class HandEvaluator
   #       FourCard of rank 2       =>  1000000 0010 0000
   #       straight flash of rank 7 => 10000000 0111 0000
   def eval_hand(hole, community)
-    return STRAIGHTFLASH | eval_straightflash(hole, community) if straightflash?(hole, community)
-    return FULLHOUSE | eval_fullhouse(hole, community) if fullhouse?(hole, community)
-    return FLASH | eval_flash(hole, community) if flash?(hole, community)
-    return STRAIGHT | eval_straight(hole, community) if straight?(hole, community)
-    return THREECARD | eval_threecard(hole, community) if threecard?(hole, community)
-    return TWOPAIR | eval_twopair(hole, community) if twopair?(hole, community)
-    return ONEPAIR | (eval_onepair(hole, community) << 4) if onepair?(hole, community)
+    cards = hole + community
+    return STRAIGHTFLASH | eval_straightflash(cards) if straightflash?(cards)
+    return FULLHOUSE | eval_fullhouse(cards) if fullhouse?(cards)
+    return FLASH | eval_flash(cards) if flash?(cards)
+    return STRAIGHT | eval_straight(cards) if straight?(cards)
+    return THREECARD | eval_threecard(cards) if threecard?(cards)
+    return TWOPAIR | eval_twopair(cards) if twopair?(cards)
+    return ONEPAIR | (eval_onepair(cards) << 4) if onepair?(cards)
     eval_holecard(hole)
   end
 
@@ -38,12 +39,11 @@ class HandEvaluator
     ranks[1] << 4 | ranks[0]
   end
 
-  def onepair?(hole, community)
-    eval_onepair(hole, community) != -1
+  def onepair?(cards)
+    eval_onepair(cards) != -1
   end
 
-  def eval_onepair(hole, community)
-    cards = hole + community
+  def eval_onepair(cards)
     rank = -1
     memo = 0 # bit memo
     for card in cards
@@ -55,19 +55,19 @@ class HandEvaluator
     return rank
   end
 
-  def twopair?(hole, community)
-    search_twopair(hole, community).size == 2
+  def twopair?(cards)
+    search_twopair(cards).size == 2
   end
 
-  def eval_twopair(hole, community)
-    ranks = search_twopair(hole, community)
+  def eval_twopair(cards)
+    ranks = search_twopair(cards)
     ranks.reduce(0) { |acc, e| acc << 4 | e }
   end
 
-  def search_twopair(hole, community)
+  def search_twopair(cards)
     ranks = []
     memo = 0
-    for card in hole + community
+    for card in cards
       mask = 1 << card.rank
       ranks = ranks.push(card.rank) if memo & mask != 0
       memo |= mask
@@ -76,16 +76,15 @@ class HandEvaluator
     return ranks.sort.reverse.take(2)
   end
 
-  def threecard?(hole, community)
-    search_threecard(hole, community) != -1
+  def threecard?(cards)
+    search_threecard(cards) != -1
   end
 
-  def eval_threecard(hole, community)
-    search_threecard(hole, community) << 4
+  def eval_threecard(cards)
+    search_threecard(cards) << 4
   end
 
-  def search_threecard(hole, community)
-    cards = hole + community
+  def search_threecard(cards)
     rank = -1
     bit_memo = cards.reduce(0) { |memo, card| memo += 1 << ((card.rank-1)*3) }
     for r in 2..14
@@ -97,16 +96,15 @@ class HandEvaluator
     return rank
   end
 
-  def straight?(hole, community)
-    search_straight(hole, community) != -1
+  def straight?(cards)
+    search_straight(cards) != -1
   end
 
-  def eval_straight(hole, community)
-    search_straight(hole, community) << 4
+  def eval_straight(cards)
+    search_straight(cards) << 4
   end
 
-  def search_straight(hole, community)
-    cards = hole + community
+  def search_straight(cards)
     bit_memo = cards.reduce(0) { |memo, card| memo |= 1 << card.rank }
 
     rank = -1
@@ -117,16 +115,15 @@ class HandEvaluator
     return rank
   end
 
-  def flash?(hole, community)
-    search_flash(hole, community) != -1
+  def flash?(cards)
+    search_flash(cards) != -1
   end
 
-  def eval_flash(hole, community)
-    search_flash(hole, community) << 4
+  def eval_flash(cards)
+    search_flash(cards) << 4
   end
 
-  def search_flash(hole, community)
-    cards = hole + community
+  def search_flash(cards)
     flash_suit = cards.group_by { |card| card.suit }
       .map { |key,vals| [key, vals.size] }
       .select { |idx, count| count >= 5 }
@@ -139,37 +136,35 @@ class HandEvaluator
     return flash_suit.reduce(-1) { |acc, suit| [acc, suit_max_rank[suit]].max }
   end
 
-  def fullhouse?(hole, community)
-    r1, r2 = search_fullhouse(hole, community)
+  def fullhouse?(cards)
+    r1, r2 = search_fullhouse(cards)
     return r1 && r2
   end
 
-  def eval_fullhouse(hole, community)
-    r1, r2 = search_fullhouse(hole, community)
+  def eval_fullhouse(cards)
+    r1, r2 = search_fullhouse(cards)
     return r1 << 4 | r2
   end
 
-  def search_fullhouse(hole, community)
-    cards = hole + community
+  def search_fullhouse(cards)
     rank_count = cards.group_by { |card| card.rank }
     three_card_rank = rank_count.select { |rank, cards| cards.size >= 3 }.keys.max
     two_pair_rank = rank_count.select { |rank, cards| cards.size >= 2 }.keys.select { |rank| rank != three_card_rank }.max
     return [three_card_rank, two_pair_rank]
   end
 
-  def straightflash?(hole, community)
-    search_straightflash(hole, community) != -1
+  def straightflash?(cards)
+    search_straightflash(cards) != -1
   end
 
-  def eval_straightflash(hole, community)
-    search_straightflash(hole, community) << 4
+  def eval_straightflash(cards)
+    search_straightflash(cards) << 4
   end
 
-  def search_straightflash(hole, community)
-    cards = hole + community
+  def search_straightflash(cards)
     flash_cards = cards.group_by { |card| card.suit }
       .select { |suit, cards| cards.size >= 5 }.values.flatten
-    search_straight(flash_cards, [])
+    search_straight(flash_cards)
   end
 
   def mask_strength(bit)
