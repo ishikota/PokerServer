85adbc21d4018bea9985be170771b97df9843479
Kota Ishimoto <ishikota086@gmail.com>
2016-04-27 17:20:30 +0900

    Implement judge method

--- app/poker_engine/game_evaluator.rb
+++ app/poker_engine/game_evaluator.rb
@@ -4,11 +4,29 @@ class GameEvaluator
     @hand_evaluator = hand_evaluator
   end
 
+  def judge(table)
+    winners = find_winner_from(table.community_card, table.seats.players)
+    prize_map = calc_prize_distribution(table.pot, winners, table.seats.players)
+    return [winners, prize_map]
+  end
+
   def find_winner_from(community_card, players)
     players.group_by { |player|
       @hand_evaluator.eval_hand(player.hole_card, community_card)
     }.max.last
   end
+
+  private
+
+    def calc_prize_distribution(pot, winners, players)
+      prize = pot.main/winners.size
+      players.each_with_index.select { |player, idx|
+        winners.include?(player)
+      }.reduce({}) { |map, player_with_idx|
+        _, idx = player_with_idx
+        map.merge( { idx => prize } )
+      }
+    end
 
 end
 
--- spec/poker_engine/game_evaluator_spec.rb
+++ spec/poker_engine/game_evaluator_spec.rb
@@ -4,12 +4,14 @@ RSpec.describe GameEvaluator do
 
   let(:table) { double("table") }
   let(:seats) { double("seats") }
+  let(:pot) { double("pot") }
   let(:players) { [] }
   let(:hand_evaluator) { double("hand evaluator") }
   let(:game_evaluator) { GameEvaluator.new(hand_evaluator) }
 
   before {
     allow(table).to receive(:community_card)
+    allow(table).to receive(:pot).and_return(pot)
     allow(table).to receive(:seats).and_return(seats)
     allow(seats).to receive(:players).and_return(players)
     allow(hand_evaluator).to receive(:eval_hand)
@@ -17,7 +19,35 @@ RSpec.describe GameEvaluator do
 
   describe "#judge" do
 
-    it "should return winner and prize distribution"
+    before {
+      3.times do |i|
+        player = double("player#{i}")
+        allow(player).to receive(:hole_card)
+        players << player
+      end
+    }
+
+    describe "without all-in player" do
+      let(:pot_amount) { 10 }
+      before {
+        allow(pot).to receive(:main).and_return(pot_amount)
+      }
+
+      describe "second player is winner" do
+
+        before {
+          allow(hand_evaluator).to receive(:eval_hand).and_return(0, 1, 0)
+        }
+
+        it "should return winner and prize distribution" do
+          winner, prize_map = game_evaluator.judge(table)
+          expect(winner.size).to eq 1
+          expect(winner).to include players[1]
+          expect(prize_map[1]).to eq pot_amount
+        end
+      end
+
+    end
 
     describe "when all-in player exists" do
 
