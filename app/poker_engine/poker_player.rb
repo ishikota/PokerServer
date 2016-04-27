a23006c5bceaa565cfe68bedc0976d549f2ce37d
Kota Ishimoto <ishikota086@gmail.com>
2016-04-27 17:44:20 +0900

    Add payinfo to PokerPlayer

--- app/poker_engine/poker_player.rb
+++ app/poker_engine/poker_player.rb
@@ -1,5 +1,5 @@
 class PokerPlayer
-  attr_reader :stack
+  attr_reader :stack, :pay_info
 
   def initialize(initial_stack)
     @stack = initial_stack
@@ -17,6 +17,37 @@ class PokerPlayer
 
   def active?
     @active
+  end
+
+  def init_pay_info
+    @pay_info = PayInfo.new
+  end
+
+  class PayInfo
+    attr_reader :amount, :status
+
+    PAY_TILL_END = 0
+    ALLIN  = 1
+    FOLDED = 2
+
+    def initialize
+      @amount = 0
+      @status = PAY_TILL_END
+    end
+
+    def update_by_pay(amount)
+      @amount += amount
+    end
+
+    def update_to_fold
+      @status = FOLDED
+    end
+
+    def update_to_allin(amount)
+      @amount += amount
+      @status = ALLIN
+    end
+
   end
 
 end
--- spec/poker_engine/poker_player_spec.rb
+++ spec/poker_engine/poker_player_spec.rb
@@ -27,5 +27,45 @@ RSpec.describe PokerPlayer do
 
   end
 
+  describe "pay_info" do
+
+    before {
+      player.init_pay_info
+    }
+
+    describe "#update" do
+
+      context "by pay more chip" do
+
+        it "should append pay amount" do
+          expect {
+            player.pay_info.update_by_pay(10)
+          }.to change { player.pay_info.amount }.by(10)
+          expect(player.pay_info.status).to eq (PokerPlayer::PayInfo::PAY_TILL_END)
+        end
+      end
+
+      context "by fold" do
+
+        it "should update state to fold" do
+          expect {
+            player.pay_info.update_to_fold
+          }.to change { player.pay_info.status }.to PokerPlayer::PayInfo::FOLDED
+        end
+      end
+
+      context "by all-in" do
+
+        it "should append pay amount and update state to ALLIN" do
+          expect {
+            player.pay_info.update_to_allin(10)
+          }.to change { player.pay_info.amount }.by(10)
+          .and change { player.pay_info.status }.to PokerPlayer::PayInfo::ALLIN
+        end
+      end
+
+    end
+  end
+
 end
 
