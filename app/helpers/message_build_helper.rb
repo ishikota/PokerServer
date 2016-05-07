21936c8e69e6d1796a96f4b0716d176060f467e1
Kota Ishimoto <ishikota086@gmail.com>
2016-05-07 13:29:12 +0900

    Create MessageBuildHelper

---
+++ app/helpers/message_build_helper.rb
@@ -0,0 +1,34 @@
+module MessageBuildHelper
+
+  module Phase
+    MEMBER_WANTED = "member_wanted"
+  end
+
+  module Type
+    WELCOME = "welcome"
+    MEMBER_ARRIVAL = "arrival"
+  end
+
+
+  def build_welcome_message
+    {}.merge(phase: Phase::MEMBER_WANTED)
+      .merge(type: Type::WELCOME)
+  end
+
+  def build_member_arrival_message(room, player)
+    {}.merge(phase: Phase::MEMBER_WANTED)
+      .merge(type: Type::MEMBER_ARRIVAL)
+      .merge(message: member_arrival_message(room, player))
+  end
+
+  private
+
+    def member_arrival_message(room, player)
+      need_player_num = room.player_num - room.players.size
+      "player [#{player.name}] arrived!!\
+      #{need_player_num} more players are needed to start the game."
+    end
+
+
+end
+
---
+++ spec/helpers/message_build_helper_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe MessageBuildHelper, :type => :helper do
+
+  describe "#build_welcome_message" do
+
+    it "should build welcome message" do
+      msg = helper.build_welcome_message
+      expect(msg[:phase]).to eq MessageBuildHelper::Phase::MEMBER_WANTED
+      expect(msg[:type]).to eq MessageBuildHelper::Type::WELCOME
+    end
+  end
+
+  describe "#build_member_arrival_message" do
+    let(:room) { double("room") }
+    let(:arrived_player) { double("player") }
+
+    before {
+      allow(arrived_player).to receive(:name).and_return "hoge"
+      allow(room).to receive(:player_num).and_return 2
+      allow(room).to receive_message_chain('players.size').and_return 1
+    }
+
+    it "should build member arrival message" do
+      msg = helper.build_member_arrival_message(room, arrived_player)
+      expect(msg[:phase]).to eq MessageBuildHelper::Phase::MEMBER_WANTED
+      expect(msg[:type]). to eq MessageBuildHelper::Type::MEMBER_ARRIVAL
+      expect(msg[:message]).to match(/hoge/)
+      expect(msg[:message]).to match(/1 more players/)
+    end
+  end
+
+end
