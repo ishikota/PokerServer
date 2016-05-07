37893cfe84ead6f09dc71e9acc818149d4341373
Kota Ishimoto <ishikota086@gmail.com>
2016-05-07 16:24:09 +0900

    Add clear_state method on Player

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,6 +2,11 @@ class Player < ApplicationRecord
   validates :name, presence: true
   validates :credential, length: { is: 22 }
 
+  def clear_state
+    update_attributes(uuid: nil)
+    EnterRoomRelationship.where(player_id: id).destroy_all
+  end
+
   # most rescent entered room
   def current_room
     my_rooms_id = EnterRoomRelationship.where(player_id: id).pluck(:id)
@@ -10,10 +15,6 @@ class Player < ApplicationRecord
 
   def take_a_seat(room)
     EnterRoomRelationship.where(player_id: id, room_id: room.id).first_or_create.touch
-  end
-
-  def leave_a_seat(room)
-    EnterRoomRelationship.where(player_id: id, room_id: room.id).destroy_all
   end
 
 end
