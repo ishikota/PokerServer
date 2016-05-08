1b2fc746a7c7e01c1231f7ea4ecf92660e607382
Kota Ishimoto <ishikota086@gmail.com>
2016-05-08 19:12:13 +0900

    Update Broadcaster to receive uuid when ask

--- spec/poker_engine/broadcaster_spec.rb
+++ spec/poker_engine/broadcaster_spec.rb
@@ -2,12 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Broadcaster do
 
-  let!(:room) { FactoryGirl.create(:room) }
+  let!(:room) { double("room") }
+  let!(:player) { FactoryGirl.create(:player) }
   let!(:server) { double('Actioncable.server') }
   let!(:broadcaster) { Broadcaster.new(server, room) }
 
   before {
     allow(server).to receive(:broadcast)
+    allow(room).to receive(:id).and_return 1
+    allow(room).to receive_message_chain('players.find_by_uuid')
+        .with(player.uuid).and_return(player)
   }
 
   describe "notification" do
@@ -22,15 +26,14 @@ RSpec.describe Broadcaster do
   end
 
   describe "ask" do
-    let(:player_id) { 1 }
 
     it "should broadcast expected message" do
-      channel = "room:#{room.id}:#{player_id}"
+      channel = "room:#{room.id}:#{player.id}"
       params = { phase: "play_poker", type: "ask", message: "hoge" }
 
       expect(server).to receive(:broadcast).with(channel, params)
 
-      broadcaster.ask(player_id, "hoge")
+      broadcaster.ask(player.uuid, "hoge")
     end
   end
 
