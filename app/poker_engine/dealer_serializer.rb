7ffd661d97dc36adb50952547c40b131e71a77fa
Kota Ishimoto <ishikota086@gmail.com>
2016-05-15 01:00:04 +0900

    Refactor

--- app/poker_engine/dealer_serializer.rb
+++ app/poker_engine/dealer_serializer.rb
@@ -10,23 +10,22 @@ module DealerSerializer
   class_methods do
     def deserialize(components_holder, json)
      dump = JSON.parse(json)
+
+     config = Marshal.load(dump["config"])
+     table = Marshal.load(dump["table"])
+
      round_manager = components_holder[:round_manager]
-     state = dump["round_manager"]
-     street = state["street"]
-     agree_num = state["agree_num"]
-     next_player = state["next_player"]
-     round_manager.set_state(street, agree_num, next_player)
+     round_manager = round_manager_from_dump(round_manager, dump)
 
      broadcaster = components_holder[:broadcaster]
      broadcaster.ask_counter = dump["broadcaster"]["ask_counter"]
-     config = Marshal.load(dump["config"])
-     table = Marshal.load(dump["table"])
 
       components_holder
         .merge!( { config: config } )
         .merge!( { table: table } )
         .merge!( { round_manager: round_manager } )
         .merge!( { broadcaster: broadcaster } )
+
       Dealer.new(components_holder, round_count=dump["round_count"])
     end
   end
